473b7b0f1c358956b82ce3260ae64b1450eaa1d0
chanyang-park <chanyang.park@nhndooray.com>
2025-02-25 11:25:27 +0900

    feat: first commit

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,104 @@
+import { useRef } from "react";
+import { StyleSheet, Text, View, Button, Alert } from "react-native";
+import { WebView } from "react-native-webview";
+
+import * as ImagePicker from "expo-image-picker";
+
+const requestPhotoPermissions = async () => {
+  // 앨범(갤러리) 접근 권한 요청
+  const { status: galleryStatus } =
+    await ImagePicker.requestMediaLibraryPermissionsAsync();
+  // 카메라 접근 권한 요청
+  const { status: cameraStatus } =
+    await ImagePicker.requestCameraPermissionsAsync();
+
+  console.log({ galleryStatus, cameraStatus });
+
+  if (galleryStatus !== "granted" || cameraStatus !== "granted") {
+    alert("카메라 및 갤러리 접근 권한이 필요합니다.");
+  }
+};
+
+const takePhotoWithCamera = async () => {
+  const result = await ImagePicker.launchCameraAsync({
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+  });
+
+  console.log("takePhotoWithCamera", result);
+
+  if (!result.canceled) {
+    console.log("📸 촬영된 이미지:", result.assets[0].uri);
+  }
+};
+
+export default function Page() {
+  const webViewRef = useRef<WebView>(null);
+
+  const handleWebViewMessage = async (event: any) => {
+    const message = JSON.parse(event.nativeEvent.data);
+
+    if (message.type === "REQUEST_CAMERA") {
+      await requestPhotoPermissions();
+      await pickImageFromGallery();
+      // await takePhotoWithCamera();
+      console.log(1111);
+    }
+  };
+
+  const sendMessageToWeb = () => {
+    const run = `
+    document.documentElement.setAttribute("data-theme", document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark");  
+  `;
+
+    webViewRef.current?.injectJavaScript(run);
+  };
+
+  const pickImageFromGallery = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true, // 편집 기능 활성화
+      aspect: [4, 3], // 비율 지정
+      quality: 1, // 품질 (0 ~ 1)
+      base64: true,
+    });
+
+    console.log("pickImageFromGallery", result);
+
+    if (!result.canceled) {
+      console.log("📷 선택된 이미지:", result.assets[0].uri);
+
+      const imageUri = `data:image/jpeg;base64,${result.assets[0].base64}`;
+
+      webViewRef.current?.postMessage(
+        JSON.stringify({
+          type: "IMAGE",
+          imageUri,
+        })
+      );
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "http://localhost:5173/" }}
+        style={{ flex: 1 }}
+        onMessage={handleWebViewMessage}
+        webviewDebuggingEnabled
+        // allowFileAccess // 파일 접근 허용
+        // allowUniversalAccessFromFileURLs // 다른 파일 접근 허용
+        // originWhitelist={["*"]} // 모든 도메인 허용
+      />
+      <Button title="Send to WebView" onPress={sendMessageToWeb} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [imageUri, setImageUri] = useState(null);
+
+  const requestCameraAccess = () => {
+    window.ReactNativeWebView?.postMessage(
+      JSON.stringify({ type: "REQUEST_CAMERA" })
+    );
+  };
+
+  useEffect(() => {
+    console.log("hihihi");
+    const handleMessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      console.log(123, { event, data });
+
+      if (data.type === "IMAGE") {
+        console.log("image", data.imageUri);
+        setImageUri(data.imageUri);
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1>React WebView</h1>
+      </div>
+      <button onClick={requestCameraAccess}>📸 사진 선택</button>
+      {imageUri ? (
+        <img
+          src={imageUri}
+          alt="Selected"
+          style={{ width: "100%", maxHeight: 300, marginTop: 20 }}
+        />
+      ) : (
+        <p>이미지를 선택해 주세요.</p>
+      )}
+    </>
+  );
+}
+
+export default App;
